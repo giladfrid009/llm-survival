57975d139504cb9d25d4b0cf9fa780a3be6b4bf5
HenDav <chend136@gmail.com>
2025-04-25 18:45:36 +0300

    Made make_multisample compatible with multigpu and consistent in terms of data produced

--- make_multisample.py
+++ make_multisample.py
@@ -1,8 +1,10 @@
-# %%
 from src import utils
 from src.rating.base import *
 from src.generation.base import *
 import torch
+
+import os
+import pickle
 
 utils.set_seed(42)
 utils.clear_memory()
@@ -15,7 +17,6 @@ import pandas as pd
 df = pd.read_json("hf://datasets/allenai/real-toxicity-prompts/prompts.jsonl", lines=True)
 
 input_dicts = df["prompt"].values.flatten().tolist()
-
 inputs = [d["text"] for d in input_dicts]
 
 # %%
@@ -27,55 +28,47 @@ rater_params = {
     "amp": True,
 }
 
-# Get the Hugging Face key.
-hf_key = utils.api_key_from_file("HF_KEY.txt")
-
 # %%
-
 generator_params = {
-    "model_name": "meta-llama/Llama-3.2-3B",
+    "model_name": "meta-llama/Llama-3.2-1B",
     "hub_token": hf_key,
     "max_input_tokens": 40,
     "max_output_tokens": 30,
     "torch_dtype": torch.bfloat16,
-    # If you need to specify the attention implementation, uncomment the line below.
-    # "attn_implementation": "flash_attention_2",
 }
 
 # %%
 # create survival analysis runner
 
-from functools import partial
 from src.survival_runner import (
     SurvivalResult,
     generate_survival_results_generic,
 )
 
 batch_size = 1500
-max_attempts = 1000
+max_attempts = 5000
+checkpoint_file = "multisample_checkpoint.pkl"
 
 survival_results = generate_survival_results_generic(
-        prompts=inputs,
-        generate_params={"batch_size": batch_size},
-        generator_params=generator_params,
-        rater_params=rater_params,
-        max_attempts=max_attempts,
-        toxicity_func="no_toxicity",
-        text_prep_func="sentence_completion",
-        conserve_memory_ratings=True,
-        multi_gpu=True,
-    )
+    prompts=inputs,
+    prompt_attempts=None,
+    generate_params={"batch_size": batch_size, "checkpoint_file": checkpoint_file},
+    generator_params=generator_params,
+    rater_params=rater_params,
+    max_attempts=max_attempts,
+    toxicity_func="no_toxicity",
+    text_prep_func="sentence_completion",
+    conserve_memory_ratings=True,
+    multi_gpu=True,
+)
 
 # %%
-# run survival analysis and print results
+# run survival analysis and collect results
 
 survival_list: list[SurvivalResult] = [res for res in survival_results]
 
 # %%
-# Save to disk the survival results
-import pickle
+# Save final results to disk
 
 with open("multisample_results.pkl", "wb") as f:
     pickle.dump(survival_list, f)
-
-
